c840a9d4a3b3d789657d1e8e971f611f657bd896
blksnk <art.jnveigel@gmail.com>
2024-01-09 16:37:11 +0100

    chore(build): edit build.sh

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -4,5 +4,6 @@ if [ -d ./lib ]; then rm -rf ./lib; fi
 # copy the contents of the src dir to the lib dir
 cp -R ./src ./lib
 
-# remove the scripts directory from lib
+# remove the scripts and test directories from lib
 rm -rf .lib/scripts
+rm -rf .lib/tests
