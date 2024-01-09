170f96903660d8a2a14c3dfc270e45e1a523c867
blksnk <art.jnveigel@gmail.com>
2024-01-09 16:40:51 +0100

    feat: first build

---
+++ lib/types/function/function.types.ts
@@ -0,0 +1,83 @@
+type FnArguments = unknown[];
+type FnReturn = unknown | void;
+
+/**
+ * Utility type to construct functions.
+ *
+ * @template {FnArguments} TArguments
+ * @template {FnReturn} TReturn
+ *
+ * @param {TArguments} [TArguments = []] - Array or tuple of function arguments. Provide {never} for no-argument functions
+ * @param {TReturn} [TReturn = void] - Function's return type.
+ * @returns {((...args: TArguments) => TReturn)}
+ *
+ * @example
+ * const add: GenericFn<[number, number], number> = (a, b) => a + b;
+ * const getUser: GenericFn<[], User> = () => (
+ *   JSON.parse(localStorage.getItem("user"))
+ * );
+ */
+export type GenericFn<
+  TArguments extends FnArguments = [],
+  TReturn extends FnReturn = void
+> = TArguments extends [] ? () => TReturn : (...args: TArguments) => TReturn;
+
+/**
+ * Utility type to construct functions that returns void.
+ * Based on {@link GenericFn}.
+ *
+ * @template {FnArguments} TArguments
+ *
+ * @param {TArguments} [TArguments = []] - Array or tuple of function arguments. Provide {never} for no-argument functions
+ * @returns {((...args: TArguments) => void)}
+ *
+ * @example
+ * const dispatchEvent: VoidFn<[string]> = (eventType) => {
+ *   // ...body
+ * };
+ * const doNothing: VoidFn = () => {};
+ */
+export type VoidFn<TArguments extends FnArguments = []> = GenericFn<
+  TArguments,
+  void
+>;
+
+/**
+ * Utility type to construct async functions have to return promises.
+ * Based on {@link GenericFn}.
+ *
+ * @template {FnArguments} TArguments
+ * @template {Exclude<FnReturn, void>} TReturn
+ *
+ * @param {TArguments} [TArguments = []] - Array or tuple of function arguments. Provide {never} for no-argument functions
+ * @param {TReturn} [TReturn = unknown] - Function's return type
+ * @returns {((...args: TArguments) => Promise<TReturn>)}
+ *
+ * @example
+ * type getDataAsync: AsyncFn<, string> = async () => "async data";
+ */
+export type AsyncFn<
+  TArguments extends FnArguments = [],
+  TReturn extends FnReturn = void
+> = GenericFn<TArguments, Promise<TReturn>>;
+
+/**
+ * Utility type to construct functions that may or may not return promises.
+ * Based on {@link GenericFn}.
+ *
+ * @template {FnArguments} TArguments
+ * @template {Exclude<FnReturn, void>} TReturn
+ *
+ * @param {TArguments} [TArguments = []] array or tuple of function arguments. Provide {never} for no-argument functions
+ * @param {TReturn} [TReturn = unknown] - Function's return type
+ * @returns {((...args: TArguments) => TReturn | Promise<TReturn>)}
+ *
+ * @example
+ *  type MaybeStringFn = MaybeAsyncFn<[], string>;
+ *  const getData: MaybeStringFn = () => "sync data";
+ *  const getDataAsync: MaybeStringFn = async () => "async data";
+ */
+export type MaybeAsyncFn<
+  TArguments extends FnArguments = [],
+  TReturn extends FnReturn = void
+> = GenericFn<TArguments, TReturn | Promise<TReturn>>;
---
+++ lib/types/function/predicate.types.ts
@@ -0,0 +1,5 @@
+import { NullishPrimitives } from "@/types/global/global.types";
+
+export type Predicate<T extends NullishPrimitives> = (
+  value: unknown
+) => value is T;
