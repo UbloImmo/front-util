b9d6359a752d9948570b276d0035399021a9b659
blksnk <art.jnveigel@gmail.com>
2024-01-17 17:13:37 +0100

    feat: typed object functions and array filter

--- src/functions/object.functions.ts
+++ src/functions/object.functions.ts
@@ -18,21 +18,97 @@ export const transformObject = <
   keyTransformer?: (key: keyof TObj & string) => TTransformedKey
 ): typeof keyTransformer extends undefined
   ? {
-      [k in keyof TObj]: TTransformedItem;
+      [Key in keyof TObj]: TTransformedItem;
     }
   : {
       [Key in TTransformedKey]: TTransformedItem;
     } => {
-  const objectEntries = Object.entries(object) as [
-    keyof TObj & string,
-    TObj[keyof TObj & string]
-  ][];
-  const transformedEntries = objectEntries.map(([key, value]) => [
-    keyTransformer ? keyTransformer(key) : key,
-    itemTransformer(value),
-  ]);
   if (keyTransformer) {
-    return Object.fromEntries(transformedEntries);
+    return objectFromEntries(
+      objectEntries(object).map(([key, value]) => [
+        keyTransformer(key),
+        itemTransformer(value),
+      ])
+    );
   }
-  return Object.fromEntries(transformedEntries);
+  return objectFromEntries(
+    objectEntries(object).map(([key, value]) => [key, itemTransformer(value)])
+  );
+};
+
+/**
+ * Returns an array of key-value pairs for the given object.
+ *
+ * @template {Record<string, unknown>} TObj
+ * @param {TObj & string} object - The object to get key-value pairs from.
+ * @return {[keyof TObj, TObj[keyof TObj & string]][]} - An array of key-value pairs.
+ */
+export const objectEntries = <TObj extends Record<string, unknown>>(
+  object: TObj
+): [keyof TObj & string, TObj[keyof TObj & string]][] =>
+  Object.entries(object) as [keyof TObj & string, TObj[keyof TObj & string]][];
+
+/**
+ * Creates an object from an array of key-value pairs.
+ * @template TKey {string}
+ * @template TValue {unknown}
+ * @param {Array<[TKey, TValue]>} entries - The array of key-value pairs.
+ * @return {{ [Key in TKey]: TValue }} - The resulting object.
+ */
+export const objectFromEntries = <TKey extends string, TValue>(
+  entries: [TKey, TValue][]
+): { [Key in TKey]: TValue } =>
+  Object.fromEntries(entries) as { [Key in TKey]: TValue };
+
+/**
+ * Returns an array of all the keys in an object.
+ *
+ * @template {Record<string, unknown>} TObj
+ * @param {TObj} object - The object from which to extract keys.
+ * @return {(keyof TObj)[]} - An array of keys from the object.
+ */
+export const objectKeys = <TObj extends Record<string, unknown>>(
+  object: TObj
+): (keyof TObj)[] => Object.keys(object) as (keyof TObj)[];
+
+/**
+ * Returns an array containing the values of all enumerable properties of the given object.
+ *
+ * @template {Record<string, unknown>} TObj
+ * @param {TObj} object - The object whose values will be returned.
+ * @return {TObj[keyof TObj & string][]} - An array containing the values of the object's properties.
+ */
+export const objectValues = <TObj extends Record<string, unknown>>(
+  object: TObj
+): TObj[keyof TObj & string][] =>
+  Object.values(object) as TObj[keyof TObj & string][];
+
+/**
+ * Filters an array based on a provided filter function.
+ * @template {unknown[]} TArr
+ * @param {TArr} array - The array to be filtered.
+ * @param {function} filterFn - The filter function used to determine which elements to keep in the array.
+ * @returns {Array} - The filtered array.
+ */
+export const arrayFilter = <TArr extends unknown[]>(
+  array: TArr,
+  filterFn: <TArrItem extends TArr[number]>(
+    item: TArrItem,
+    index: number,
+    array: TArrItem[]
+  ) => boolean
+): {
+  [Index in keyof TArr]: Index extends number
+    ? ReturnType<typeof filterFn<TArr[Index]>> extends true
+      ? TArr[Index]
+      : never
+    : TArr[Index];
+} => {
+  return [...array].filter(filterFn) as TArr as {
+    [Index in keyof TArr]: Index extends number
+      ? ReturnType<typeof filterFn<TArr[Index]>> extends true
+        ? TArr[Index]
+        : never
+      : TArr[Index];
+  };
 };
