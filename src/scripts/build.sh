2410f33ab7b2eb81603822be8f2e823f7e91ef20
blksnk <art.jnveigel@gmail.com>
2024-01-17 11:44:28 +0100

    chore: add logs to build script

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,9 +1,19 @@
+echo "Exporting select source files to lib..."
+
 # delete existing lib directory if it exists
 if [ -d ./lib ]; then rm -rf ./lib; fi
+
+echo "  Removed existing lib directory"
 
 # copy the contents of the src dir to the lib dir
 cp -R ./src ./lib
 
+echo "  Copied src directory to new lib directory"
+
 # remove the scripts and test directories from lib
 rm -rf ./lib/scripts
 rm -rf ./lib/tests
+
+echo "  Removed scripts and test directories from lib directory"
+
+echo "Done"
