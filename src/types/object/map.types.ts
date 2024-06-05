4a6e79b607c9e060683f47e250def34e3f57ac8c
blksnk <art.jnveigel@gmail.com>
2024-06-06 01:30:41 +0200

    feat: object replace type

--- src/types/object/map.types.ts
+++ src/types/object/map.types.ts
@@ -9,3 +9,24 @@
 export type ValueMap<TKeys extends string, TValue> = {
   [TKey in TKeys]: TValue;
 };
+
+/**
+ * Replaces an source objects property with a target objects matching property's type.
+ * If a fourth argument is provided, it will be used as the target object's key
+ *
+ * @template {object} TSource - The source object
+ * @template {keyof TSource & string} TSourceKey - The source object's key
+ * @template {object} TTarget - The target object
+ * @template {keyof TTarget & string} TTargetKey - The target object's key
+ */
+export type Replace<
+  TSource extends object,
+  TSourceKey extends keyof TSource & string,
+  TTarget extends object,
+  TTargetKey extends (keyof TTarget & string) | TSourceKey = TSourceKey
+> = Omit<TSource, TSourceKey> &
+  (TTargetKey extends TSourceKey & keyof TTarget & string
+    ? Pick<TTarget, TTargetKey>
+    : {
+        [Key in TSourceKey]: TTarget[TTargetKey & keyof TTarget & string];
+      });
